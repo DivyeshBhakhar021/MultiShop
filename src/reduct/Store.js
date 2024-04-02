3211d764138c1b628bbc59a775ef648a4d8263a0
DivyeshBhakhar021 <divyeshbhakhar021@gmail.com>
2024-04-02 13:17:55 +0530

    loding

--- src/reduct/Store.js
+++ src/reduct/Store.js
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from "redux"
 import { rootReducer } from "./reducer"
 import { thunk } from "redux-thunk"
 
-export const configestore = () =>{
+export const configestore = () => {
     const store = createStore(rootReducer, applyMiddleware(thunk))
     return store
 } 
--- src/reduct/reducer/index.js
+++ src/reduct/reducer/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { facilitiesReducer } from "./facilities_reducer";
 
-export const rootReducer = combineReducers({    
-    facilities :facilitiesReducer
+export const rootReducer = combineReducers({
+    facilities: facilitiesReducer
 })
